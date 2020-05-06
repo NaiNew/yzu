ccb34b6d7315467c34e707848fadc04124425296
LE-S34 <r208790@yahoo.com.tw>
2020-05-06 21:21:48 +0800

    python

--- lesson05/json Demo2.py
+++ lesson05/json Demo2.py
@@ -1,12 +1,13 @@
 import json
 import requests
 
-url = 'https://data.coa.gov.tw/Service/OpenData/FromM/AgricultureiRiceFailure.aspx'
+#url = 'https://data.coa.gov.tw/Service/OpenData/FromM/AgricultureiRiceFailure.aspx'  # 不合格
+url = 'https://data.coa.gov.tw/Service/OpenData/FromM/AgricultureiRiceQualified.aspx'  # 合格
 r = requests.get(url)
 # print(r.status_code)
 # print(r.text)
 bad_rices = json.loads(r.text)
 for bad in bad_rices:
-    if '新' in bad.get ('品名'):
+    if '新生' in bad.get ('品名'):
         print(bad.get('品名'), bad.get('不合格原因'))
 
